1654a0ef8a15e478f32b287a8f1bf644eb102ee8
Juhun <wngns8334@gmail.com>
2024-11-01 18:59:20 +0900

    기본 세팅

---
+++ CarruDriver/CarruDriver/CarruDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarruDriverApp.swift
+//  CarruDriver
+//
+//  Created by 이주훈 on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CarruDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
